15bf9022f1d71939b60152ccca8450469d5f93a1
Alexandre LECUT <alexandre.lecut@ama.bzh>
2024-06-04 16:04:32 +0200

    fix: release camera onPause

--- app/src/main/java/com/example/webviewsample/MainActivity.kt
+++ app/src/main/java/com/example/webviewsample/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.webviewsample
 
 import android.os.Bundle
+import android.util.Log
 import android.webkit.PermissionRequest
 import android.webkit.WebChromeClient
 import android.webkit.WebView
@@ -9,26 +10,36 @@ import androidx.activity.ComponentActivity
 
 
 class MainActivity : ComponentActivity() {
+
+    private var myWebView: WebView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val myWebView = WebView(this)
-        myWebView.settings.javaScriptEnabled = true
-        myWebView.getSettings().setJavaScriptEnabled(true);
+        myWebView = WebView(this)
+        myWebView!!.settings.javaScriptEnabled = true
+        myWebView!!.getSettings().setJavaScriptEnabled(true);
         WebView.setWebContentsDebuggingEnabled(true);
-        myWebView.getSettings().mediaPlaybackRequiresUserGesture = false;
+        myWebView!!.getSettings().mediaPlaybackRequiresUserGesture = false;
 
-        myWebView.setWebViewClient(WebViewClient())
-        myWebView.setWebChromeClient(object : WebChromeClient() {
+        myWebView!!.setWebViewClient(WebViewClient())
+        myWebView!!.setWebChromeClient(object : WebChromeClient() {
             override fun onPermissionRequest(request: PermissionRequest) {
                 request.grant(request.resources)
             }
         })
 
         val cameraInterface = JavaScriptInterface(this)
-        myWebView.addJavascriptInterface(cameraInterface, "Camera")
+        myWebView!!.addJavascriptInterface(cameraInterface, "Camera")
 
-        myWebView.loadUrl("file:///android_asset/index.html")
+        myWebView!!.loadUrl("file:///android_asset/index.html")
         setContentView(myWebView)
+    }
+
+    override fun onPause() {
+        myWebView!!.evaluateJavascript("(function() { window.dispatchEvent(stopCameraEvent); })();"
+        ) {
+            Log.e("stopCamera", "js event received")
+        }
+        super.onPause()
     }
 }
