e228cd4e47ec72ad696b2d882f54aa585c3d074b
James Ashton <james.ashton@realwear.com>
2024-06-07 15:00:43 +0100

    Fixed camera load

--- app/src/main/java/com/example/webviewsample/MainActivity.kt
+++ app/src/main/java/com/example/webviewsample/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.webviewsample
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.os.SystemClock
 import android.util.Log
+import android.view.MotionEvent
 import android.webkit.PermissionRequest
 import android.webkit.WebChromeClient
 import android.webkit.WebView
@@ -15,22 +19,26 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        myWebView = WebView(this)
-        myWebView!!.settings.javaScriptEnabled = true
-        myWebView!!.getSettings().setJavaScriptEnabled(true);
-        WebView.setWebContentsDebuggingEnabled(true);
-        myWebView!!.getSettings().mediaPlaybackRequiresUserGesture = false;
-
-        myWebView!!.setWebViewClient(WebViewClient())
-        myWebView!!.setWebChromeClient(object : WebChromeClient() {
-            override fun onPermissionRequest(request: PermissionRequest) {
-                request.grant(request.resources)
+        myWebView = WebView(this).apply {
+            settings.mediaPlaybackRequiresUserGesture = false
+            WebView.setWebContentsDebuggingEnabled(true)
+            settings.javaScriptEnabled = true
+            webViewClient = object : WebViewClient() {
+                override fun onPageFinished(view: WebView?, url: String?) {
+                    super.onPageFinished(view, url)
+                    // Simulate a click after 2 seconds
+                    simulateClickAfterDelay()
+                }
             }
-        })
-
+            webChromeClient = object : WebChromeClient() {
+                override fun onPermissionRequest(request: PermissionRequest) {
+                    request.grant(request.resources)
+                }
+            }
+        }
         val cameraInterface = JavaScriptInterface(this)
         myWebView!!.addJavascriptInterface(cameraInterface, "Camera")
-
+        // Load the local HTML page
         myWebView!!.loadUrl("file:///android_asset/index.html")
         setContentView(myWebView)
     }
@@ -49,5 +57,33 @@ class MainActivity : ComponentActivity() {
             Log.e("stopCamera", "js event received")
         }
         super.onPause()
+    }
+
+    private fun simulateClickAfterDelay() {
+        Handler(Looper.getMainLooper()).postDelayed({
+            myWebView?.let { webView ->
+
+                val eventTime = SystemClock.uptimeMillis()
+                val x = webView.width / 2f // Center X
+                val y = webView.height / 2f // Center Y
+
+
+                val downEvent = MotionEvent.obtain(
+                    eventTime, eventTime, MotionEvent.ACTION_DOWN, x, y, 0
+                )
+
+                val upEvent = MotionEvent.obtain(
+                    eventTime, eventTime + 100, MotionEvent.ACTION_UP, x, y, 0
+                )
+
+                webView.dispatchTouchEvent(downEvent)
+                webView.dispatchTouchEvent(upEvent)
+
+                downEvent.recycle()
+                upEvent.recycle()
+
+                Log.d("WebViewSample", "Simulated click at center of WebView")
+            }
+        }, 500) // Delay of .5 seconds
     }
 }
